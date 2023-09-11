be8b5e52df1b0ff4b4ec6222e9f0ab5d2ee5bc6b
Anuja-Aliveli <133123427+Anuja-Aliveli@users.noreply.github.com>
2023-09-11 10:47:10 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,17 +1,13 @@
 let successfulOperations = 0;
 let totalOperations = 0;
 
-// Function to update the UI with success rate
 const updateSuccessRate = () => {
   const successRate = (successfulOperations / totalOperations) * 100;
-  // Update your UI element with the success rate (e.g., a <div> or <span>)
   const successRateElement = document.getElementById("success-rate");
   successRateElement.textContent = `Success Rate: ${successRate.toFixed(2)}%`;
 };
 
-// Function to insert rows into the data table
 const insertRow = (data) => {
-  // Replace with your UI update logic
   const table = document.getElementById("data-table");
   const row = table.insertRow();
   const nameCell = row.insertCell(0);
@@ -23,24 +19,18 @@ const insertRow = (data) => {
   originCell.textContent = data.origin;
   destinationCell.textContent = data.destination;
   timestampCell.textContent = new Date(data.timestamp).toLocaleString();
-
-  // Increment the total operations counter
   totalOperations++;
-
-  // Check if the data processing was successful (you can modify this condition)
-  const processingSuccessful = true; // Replace with your condition
+  const processingSuccessful = true; 
   if (processingSuccessful) {
     successfulOperations++;
   }
-
-  // Update the success rate in the UI
   updateSuccessRate();
 };
 
 // Function to fetch data from the API
 const fetchData = async () => {
   try {
-    const response = await fetch("http://localhost:5000/timeseries"); // Replace with your API endpoint
+    const response = await fetch("http://localhost:5000/timeseries");
     if (response.ok) {
       const data = await response.json();
       const item = data[data.length - 1];
@@ -53,8 +43,5 @@ const fetchData = async () => {
   }
 };
 
-// Fetch data initially
 fetchData();
-
-// Fetch data every 10 seconds
 setInterval(fetchData, 10000);
